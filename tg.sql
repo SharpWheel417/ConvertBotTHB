a6a64d412deaf6886a6f243ec0bfbd544111811b
SharpWheel417 <kotejackwhite@icloud.com>
2024-03-08 11:45:58 +0700

    stash

--- tg.sql
+++ tg.sql
@@ -1,7 +1,5 @@
 -- Adminer 4.8.1 PostgreSQL 16.1 (Debian 16.1-1.pgdg120+1) dump
 
-\connect "tg";
-
 DROP TABLE IF EXISTS "banks";
 DROP SEQUENCE IF EXISTS banks_id_seq;
 CREATE SEQUENCE banks_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
